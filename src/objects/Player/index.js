082ab88854cfa1af6d7062c6dfc0890642094dba
lucasluz99 <lucasluzthatsme@gmail.com>
2019-09-19 17:37:28 -0300

    css do Player

--- src/objects/Player/index.js
+++ src/objects/Player/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
-
+import './styles.css';
+import playerX from '../../img/player-x.png'
 const Player = () =>{
-    return(<button className="player">X</button>);
+    return(<button className="player">
+        <img src={playerX} alt="Jogador X"></img>
+    </button>);
 }
 export default Player;
