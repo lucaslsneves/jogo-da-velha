89a25162257f260331f27d91f89c122e2e7f8642
lucasluz99 <lucasluzthatsme@gmail.com>
2019-09-19 13:58:11 -0300

    começo da arquitetura de pastas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/setting/colors.css'
+import './styles/generic/reset.css'
+import './styles/elements/base.css'
 import App from './App';
 
 
