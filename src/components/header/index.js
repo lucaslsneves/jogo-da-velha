69992cf5df64dcb3ab7063537ac727bc5cf21703
lucasluz99 <lucasluzthatsme@gmail.com>
2019-09-19 16:41:04 -0300

    css do header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Logo from '../../objects/Logo';
 import Sobre from '../../objects/Sobre'
 import Menu from '../../objects/Menu';
+import './styles.css';
 
 const Header = () =>{
     return(
