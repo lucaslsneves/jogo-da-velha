1e8e30067acb0ff71d0ea2ae1fd359be9a14cb72
lucasluz99 <lucasluzthatsme@gmail.com>
2019-09-19 15:21:28 -0300

    Object logo criado e adiciando no app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import Sobre from './objects/Sobre'
-
+import Sobre from './objects/Sobre';
+import Logo from './objects/Logo';
 function App() {
   return (
-    <Sobre/>
+   <>
+      <Logo/>
+      <Sobre/>
+   </>
   );
 }
 
