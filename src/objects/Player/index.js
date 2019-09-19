b19b72eaa7d28628cb30a420d13980a9eeb0aeeb
lucasluz99 <lucasluzthatsme@gmail.com>
2019-09-19 17:57:29 -0300

    aceitar os jogadores no player

--- src/objects/Player/index.js
+++ src/objects/Player/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './styles.css';
 import playerX from '../../img/player-x.png'
-const Player = () =>{
+import playerO from '../../img/player-circle.png';
+
+const Player = ({player}) =>{
+    const players = [];
+    players['x'] = playerX;
+    players['o'] = playerO;
+    
     return(<button className="player">
-        <img src={playerX} alt="Jogador X"></img>
+        <img src={players[player]} alt={`Jogador ${player.toUpperCase()}`}></img>
     </button>);
 }
 export default Player;
