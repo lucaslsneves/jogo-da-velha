532bcc293660145fecf19c30132bd35ce7915593
lucasluz99 <lucasluzthatsme@gmail.com>
2019-09-19 15:43:26 -0300

    criado o menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Sobre from './objects/Sobre';
 import Logo from './objects/Logo';
+import Menu from './objects/Menu';
 function App() {
   return (
    <>
       <Logo/>
       <Sobre/>
+      <Menu/>
    </>
   );
 }
---
+++ src/objects/Menu/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css';
+
+const Menu = () =>{
+    return(<a className="menu" href="#menu"><span className="center">Menu</span></a>)
+};
+
+export default Menu;
