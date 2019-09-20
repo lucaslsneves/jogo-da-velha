eed4ffa9656c041106dfd0d7b218be6b3a0027e5
lucasluz99 <lucasluzthatsme@gmail.com>
2019-09-20 15:04:04 -0300

    criado o arquivo app.css

--- src/objects/Logo/index.js
+++ src/objects/Logo/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import CollabCode from '../../img/logo.png'
+import './styles.css';
 const Logo = () =>{
-    return(<img src={CollabCode} alt="Logo da CollabCode"/>)
+    return(<img className="logo" src={CollabCode} alt="Logo da CollabCode"/>)
 }
 
 export default Logo;
