719abdf25a76e31e28ed44272c1b18b134d64ed4
lucasluz99 <lucasluzthatsme@gmail.com>
2019-09-22 23:00:52 -0300

    iniciado o css do iconClose

--- src/objects/iconClose/index.js
+++ src/objects/iconClose/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles.css';
 const iconClose = () => (
-<a class="icon-close">Fechar</a>
+<a class="icon-close"><span class="content">Fechar</span></a>
 );
 
 export default iconClose;
