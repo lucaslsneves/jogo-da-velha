4c455816bf418deeae4e33822ba28694f020ffd2
lucasluz99 <lucasluzthatsme@gmail.com>
2019-09-22 21:53:48 -0300

    feito o object About

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import Header from './components/Header';
 import Hashtag from './components/Hashtag';
 import Checkbox from './objects/Checkbox';
+import About from './objects/About';
 const App = () => {
   return (
     <main className="app">
     <Header/>
     <Hashtag/>
     <Checkbox id = "checkbox" value = "show" content = "Mostar eventos"/>
-    
-    
+    <About/>
     </main>
 
   );
---
+++ src/objects/About/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const About = () => (
+    <article className="about">Lucas Luz</article>
+)
+    
+;
+
+export default About;
