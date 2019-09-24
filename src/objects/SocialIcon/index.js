72350990398540a6f6436b9dc56ccbc139eb3399
lucasluz99 <lucasluzthatsme@gmail.com>
2019-09-24 00:10:21 -0300

    finalizado o UserProfile

--- src/objects/SocialIcon/index.js
+++ src/objects/SocialIcon/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const SocialIcon = ({src = "",alt = "",href =""}) => (
-    <a href={href}><img className="social-icon" src={src} alt={alt}/></a>
+    <a target="_blank" href={href}><img className="social-icon" src={src} alt={alt}/></a>
 );
 
 export default SocialIcon;
