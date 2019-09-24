6926f00f0f713235bded9cf333128734934c9399
lucasluz99 <lucasluzthatsme@gmail.com>
2019-09-23 22:55:55 -0300

    html e css do object AvatarProfile

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Hashtag from './components/Hashtag';
 import Checkbox from './objects/Checkbox';
 import About from './objects/About';
 import InternalHeader from './components/InternalHeader';
+import AvatarProfile from './objects/AvatarProfile';
+import AvatarLucas from './img/lucas.jpeg';
+
 const App = () => {
   return (
     <main className="app">
@@ -13,6 +16,7 @@ const App = () => {
     <Checkbox id = "checkbox" value = "show" content = "Mostar eventos"/>
     <About>
         <InternalHeader/>
+        <AvatarProfile src={AvatarLucas} alt="Avatar Lucas"/>
      </About>  
     </main>
 
---
+++ src/objects/AvatarProfile/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.css';
+
+const AvatarProfile = ({src="" ,alt=""}) =>(
+    <img className="avatar-profile" src={src} alt={alt}/>
+
+)
+
+
+
+export default AvatarProfile;
