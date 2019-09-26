2c8760715702e5f344cc9ebc339b670e29683af7
lucasluz99 <lucasluzthatsme@gmail.com>
2019-09-26 17:07:52 -0300

    criado o controle de proximo jogador

--- src/components/HashtagGame/index.js
+++ src/components/HashtagGame/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React,{useState} from 'react';
 import CardGame from '../../objects/CardGame';
 import PlayerGame from '../../objects/PlayerGame';
 import './styles.css';
 
+
+
 const HashtagGame = () =>{
+    const [nextPlayer,setNextPlayer] = useState("x");
+    
+    const handleClick  = () => {
+        console.log("proximo jogador",nextPlayer);
+        setNextPlayer((old) =>{
+             if(old === "x"){
+                 return "o";
+             }else{
+                 return "x";
+             }
+             
+        })
+    };
     return(
     <CardGame>
-        <ul className="hashtag-game">
+        <ul className="hashtag-game" onClick={handleClick}>
              <li className="item"><PlayerGame/></li>
              <li className="item"><PlayerGame/></li>
              <li className="item"><PlayerGame /></li>
