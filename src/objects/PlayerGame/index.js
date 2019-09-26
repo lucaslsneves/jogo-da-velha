1bd76459a36670f1b4c8fc27e36522075ed14407
lucasluz99 <lucasluzthatsme@gmail.com>
2019-09-26 10:33:55 -0300

    o object PlayerGame agora pode ficar vazio (estado inicial do tabuleiro)

--- src/objects/PlayerGame/index.js
+++ src/objects/PlayerGame/index.js
@@ -3,13 +3,13 @@ import './styles.css';
 import playerX from '../../img/player-x.png'
 import playerO from '../../img/player-circle.png';
 
-const PlayerGame = ({player}) =>{
+const PlayerGame = ({player = false}) =>{
     const players = [];
     players['x'] = playerX;
     players['o'] = playerO;
     
     return(<button className="player-game">
-        <img src={players[player]} alt={`Jogador ${player.toUpperCase()}`}></img>
+       {player && <img src={players[player]} alt={`Jogador ${player.toUpperCase()}`}></img>}
     </button>);
 }
 export default PlayerGame;
