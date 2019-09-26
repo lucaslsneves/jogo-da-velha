9382da60d476347ab8ed84cecedf3137e8b907ec
lucasluz99 <lucasluzthatsme@gmail.com>
2019-09-26 10:42:46 -0300

    adicionado o evento de click ao object PlayerGame

--- src/objects/PlayerGame/index.js
+++ src/objects/PlayerGame/index.js
@@ -3,12 +3,17 @@ import './styles.css';
 import playerX from '../../img/player-x.png'
 import playerO from '../../img/player-circle.png';
 
+
+const handleClick = () =>{
+    console.log("oi");   
+}
+
 const PlayerGame = ({player = false}) =>{
     const players = [];
     players['x'] = playerX;
     players['o'] = playerO;
     
-    return(<button className="player-game">
+    return(<button onClick={handleClick} className="player-game">
        {player && <img src={players[player]} alt={`Jogador ${player.toUpperCase()}`}></img>}
     </button>);
 }
