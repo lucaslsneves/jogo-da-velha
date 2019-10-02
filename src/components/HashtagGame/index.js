0994e982256fe50ac0e660afd92e8560a2b03f5c
lucasluz99 <lucasluzthatsme@gmail.com>
2019-10-02 02:32:19 -0300

    Máquina do Tempo

--- src/components/HashtagGame/index.js
+++ src/components/HashtagGame/index.js
@@ -1,49 +1,16 @@
-import React,{useState} from 'react';
+import React from 'react';
 import CardGame from '../../objects/CardGame';
 import PlayerGame from '../../objects/PlayerGame';
 import './styles.css';
 
 
 
-const HashtagGame = ({callback}) =>{
-    const [nextPlayer,setNextPlayer] = useState("x");
-    const [lastRound,setLastRound] = useState(0);
-    const [history,setHistory] = useState(
-    [
-        {
-        round:0,
-        state:[
-         {id:1,content: ''},
-         {id:2,content: ''},
-         {id:3,content: ''},
-         {id:4,content: ''},
-         {id:5,content: ''},
-         {id:6,content: ''},
-         {id:7,content: ''},
-         {id:8,content: ''},
-         {id:9,content: ''}]
-       }
-   ]
-     );
-  
-    
-    const handleClick  = (id) => {
-            setHistory(old => [...old,{
-                round:lastRound +1,
-                state:old[lastRound].state.map(player => player.id === id  ? {id,content:nextPlayer} : player)
-            }])
-            setLastRound(old => ++old)
-            callback(nextPlayer);
-            setNextPlayer(old => old === "x" ? "o" : "x");     
-           
-    }
-
-    return(
+const HashtagGame = ({historyGame,onClick,lastRound}) =>
+   (
     <CardGame>
-         
         <ul className="hashtag-game" >
-           {history[lastRound].state.map(({id,content}) => 
-                <li key={id} onClick={() => content === '' && handleClick(id)} className="item">
+           {historyGame[lastRound].state.map(({id,content}) => 
+                <li key={id} onClick={() => content === '' && onClick(id)} className="item">
                     <PlayerGame id={id} content= {content}/>
                 </li> 
             )}  
@@ -51,6 +18,6 @@ const HashtagGame = ({callback}) =>{
     </CardGame>
     )
     
-}
+
 
 export default HashtagGame;
--- src/components/WrapperHashtagHistory/index.js
+++ src/components/WrapperHashtagHistory/index.js
@@ -8,21 +8,57 @@ const WrapperHashtagHistory = () =>{
     
 const [history, setHistory] = useState(["Início"]);
 const [active,setActive] = useState(false);
+const [nextPlayer,setNextPlayer] = useState("x");
+const [lastRound,setLastRound] = useState(0);
+   
+const [historyGame,setHistoryGame] = useState(
+    [
+        {
+        round:0,
+        state:[
+         {id:1,content: ''},
+         {id:2,content: ''},
+         {id:3,content: ''},
+         {id:4,content: ''},
+         {id:5,content: ''},
+         {id:6,content: ''},
+         {id:7,content: ''},
+         {id:8,content: ''},
+         {id:9,content: ''}]
+       }
+   ]
+     );
+
 const addHistory = (player,id) => {
     setHistory(old => [...old, `Adicionou ${player.toUpperCase()}`]);
   };
 
 const changeHistory = key =>{
-    setHistory(old => history.slice(0,key+1))
+    setLastRound(key);
+    setHistory(old => old.slice(0,key+1))
+    setHistoryGame(old => old.slice(0,key+1));
+       
+    
   }
   const showHideHistory = () =>{
     setActive((old) => old ? false : true);
     
 }
+ 
+const handleClick  = (id) => {
+    setHistoryGame(old => [...old,{
+        round:lastRound +1,
+        state:old[lastRound].state.map(player => player.id === id  ? {id,content:nextPlayer} : player)
+    }])
+    setLastRound(old => ++old)
+    addHistory(nextPlayer);
+    setNextPlayer(old => old === "x" ? "o" : "x");     
+   
+}
     
     return(
     <div className={`wrapper-hashtagHistory ${active ? `-active` : ``}`}>
-        <HashtagGame callback={addHistory} />
+        <HashtagGame lastRound={lastRound} historyGame={historyGame} onClick={handleClick} />
         <InputCheckbox onClick={showHideHistory} id="checkbox" value="show" content="Mostar eventos" />
         <HistoryGame onClick={changeHistory} history={history} />
     </div>
